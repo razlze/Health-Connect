51cb3597716a6af66d10b24e386e6bbbdaca5708
razlze <razisyed03@gmail.com>
2021-08-14 16:22:36 -0400

    Co-authored-by: mufengl <mufengl@users.noreply.github.com> Co-authored-by: topcheese044 <topcheese044@users.noreply.github.com> Co-authored-by: Jamie Tsai <JamieTsai1024@users.noreply.github.com>

--- forms.py
+++ forms.py
@@ -3,7 +3,8 @@ from wtforms import SelectField, IntegerField, SubmitField
 from wtforms.validators import DataRequired
 
 class PreferencesForm(FlaskForm):
+  #creating input fields for form
   province = SelectField('Province', choices=[("ab", "Alberta"), ("bc", "British Columbia"), ("mb", "Manitoba"), ("nb", "New Brunswick"), ("nl", "Newfoundland & Labrador"), ("nt", "Northwest Territories"), ("ns", "Nova Scotia"), ("nu", "Nunavut"), ("on", "Ontario"), ("pei", "Prince Edward Island"), ("qu", "Quebec"), ("sk", "Saskatchewan"), ("yt", "Yukon Territories")], validators=[DataRequired()])
   facility = SelectField('Facility', choices=[("Ambulatory health care services", "Ambulatory care (including clinics)"), ("Hospitals", "Hospitals"), ("Nursing and residential care facilities", "Nursing & residential")], validators=[DataRequired()])
-  wait_time = IntegerField('Wait Time', validators=[DataRequired()])
+  wait_time = IntegerField('Max Wait Time (Hours)', validators=[DataRequired()])
   submit = SubmitField('Filter')
--- main.py
+++ main.py
@@ -12,107 +12,76 @@ SECRET_KEY = os.urandom(32)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 
-def total_time(row):
-  me = geocoder.ip('me')
-  # print(me.latlng)  
-  my_coords = (me.latlng[0], me.latlng[1])
+def drive_time(row):
+  # me = geocoder.ip('me')
+  # my_coords = (me.latlng[0], me.latlng[1])
+  my_coords = (45, -75)
   destination_coords = (row["latitude"], row["longitude"]) 
   distances = distance.distance(my_coords, destination_coords).km
-  # print(distance.distance(my_coords, destination_coords).km)
-  # print("type row['wait_time']", type(row["wait_time"]))
-  # print("type distance/60", type(distances/60))
-  totalTime = distances/60 + row["wait_time"]
-  return totalTime
+  driveTime = distances / 60 
+  return int(driveTime)
+
+def total_time(row):
+  return int(row["wait_time"] + row["drive_time"])
 
 def wait_time(row):
-  return int(random.randint(60,600))
+  return int(random.randint(10,300))
 
 @app.route('/') 
 @app.route('/main')
 def main():
   return render_template('home.html')
 
-@app.route('/application')
+@app.route('/application', methods=['GET', 'POST'])
 def application(): 
-
-  # location = request.form["location"]
-  df = pd.read_csv('blah.csv') 
-  print(df)
-  # path.append(df)
-  # path.append(list(df.columns))
-  # path.append(list(df["province"]))
-  # path.append(df.head(3))
-
-  filtered = df[df['province'].str.match('')] 
-  filtered = filtered[(filtered['longitude'] != "null")]
-  filtered = filtered[(filtered['latitude'] != "null")]
-  print("after cleaning", filtered) 
-
-  filtered["wait_time"] = filtered.apply(wait_time, axis=1)
-  print("wait time before", filtered) 
-  filtered["total_time"] = filtered.apply(total_time, axis=1)
-  print("wait time after", filtered)
-
-  # Delete columns 
-  # del filtered["index"]
-  # del filtered["longitude"]
-  # del filtered["latitude"]
-
-  # filtered.filter(items=["facility_name", "odhf_facility_type", "postal_code", "wait_time", "total_time"])
-  filtered.filter(items=["facility_type", "facility_name", "province", "longitude", "latitude", "wait_time", "total_time"])
-  del filtered['index']
-  print("filtered HERE\n", filtered) 
-
-  print("filtered.values.toList()", filtered.values.tolist())   
-  print("Check")
-  return render_template("application.html", hasData=True, column_names=["Facility Type", "Facility Name", "Postal Code", "Wait Time", "Total Time"], row_data=list(filtered.values.tolist()))
-
-@app.route("/preferences", methods=['GET', 'POST'])
-def preferences():
   form = PreferencesForm()
+  # button press
   if form.validate_on_submit():
+
+    # Get the variables from the form
     result = request.form
-    print("result", result)
     province = result["province"]
     facility = result["facility"]
-    waitTime = int(result["wait_time"])*60
-    print("Wait time", waitTime)
-    print("Province", province)
+    waitTime = int(result["wait_time"]) * 60
 
+    # Get CSV
     df = pd.read_csv('blah.csv') 
 
-    # Get all hospitals from the requested province, facility type, wait time 
-    filtered = df[df['province'].str.match(province)] 
-    print("haha 1", filtered)
+    # Filter for hospitals from the requested province and facility type
+    df = df[df['province'].str.match(province)] 
+    df = df[df['facility_type'].str.match(facility)] 
 
-    filtered = filtered[filtered['facility_type'].str.match(facility)] 
-    print("haha 2", filtered)
-    # Get all hospitals from the requested wait time 
-    filtered["wait_time"] = filtered.apply(wait_time, axis=1)
-
-    print("haha 3", filtered)
     # Get rid of null longitudes and latitudes
-    filtered = filtered[(filtered['longitude'] != "null")]
-    filtered = filtered[(filtered['latitude'] != "null")]
-    filtered = filtered[filtered['wait_time'] <= waitTime] 
-    # Filling in wait time (not random we promise) 
-    print("wait time before", filtered) 
-    # Get all hostpitals with wait times lower than the requested one 
-    
-    
-    print("wait time after", filtered)
-    print("num", len(filtered))
-    if len(filtered) == 0:
-      print("no data")
-      return render_template("application.html", hasData=False)
+    df = df[(df['longitude'] != "null")]
+    df = df[(df['latitude'] != "null")]
+
+    # Fill out and filter wait time 
+    df["wait_time"] = df.apply(wait_time, axis=1)
+
+    # Filter wait time 
+    df = df[df['wait_time'] <= waitTime] 
+
+    # Check if the dataframe is empty 
+    if len(df) == 0:    
+      # Return the template 
+      return render_template("application.html", hasNoData=True, form=form, column_names=["Facility Type", "Facility Name", "Province", "Wait Time", "Drive Time", "Total Time"], row_data=list())
     else: 
       # Filling in total time 
-      filtered["total_time"] = filtered.apply(total_time, axis=1)
-      print("before deleteing index", filtered)
-      del filtered["index"]
-      print("after deleteing index", filtered)
-      filtered.sort_values(by=["total_time"]) 
-
-      return render_template("application.html", hasData=True, column_names=["Facility Type", "Facility Name", "Postal Code", "Wait Time", "Total Time"], row_data=list(filtered.values.tolist()))
-    
-  return render_template('preferences.html', title='Preferences', form=form)
+      df["drive_time"] = df.apply(drive_time, axis=1)
+      df["total_time"] = df.apply(total_time, axis=1)
+      del df["index"]
+      df=df.sort_values(by=["total_time"]) 
+      return render_template("application.html", hasNoData=False, form=form, column_names=["Facility Type", "Facility Name", "Province", "Wait Time", "Drive Time", "Total Time"], row_data=list(df.values.tolist()))
+  
+  # Get CSV
+  df = pd.read_csv('blah.csv') 
+  # Fill out wait time and total time 
+  df["wait_time"] = df.apply(wait_time, axis=1)
+  df["drive_time"] = df.apply(drive_time, axis=1)
+  df["total_time"] = df.apply(total_time, axis=1)
+  # Delete the index column
+  del df["index"]
+  # Sort by total time 
+  df=df.sort_values(by=["total_time"]) 
+  # Return the template 
+  return render_template('application.html', hasNoData=False, form=form, column_names=["Facility Type", "Facility Name", "Province", "Wait Time", "Drive Time", "Total Time"], row_data=list(df.values.tolist()))
